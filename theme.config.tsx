f72d4868ac03743d760f08f9a65c9fabd727f464
Callum McIntyre <mcintyre1994@gmail.com>
2023-01-20 11:29:49 +0000

    Remove unused content + tweak theme config

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,17 +2,17 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Taking Payments IRL with Solana Pay</span>,
   project: {
     link: 'https://github.com/shuding/nextra-docs-template',
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/mcintyre94/nextra-pay-tutorial',
   footer: {
     text: 'Nextra Docs Template',
   },
+  search: {
+    placeholder: "Search tutorial...",
+  }
 }
 
 export default config
